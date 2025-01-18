8f5fd8ff78ab14b292038cc6cc68a1c83c61e9ca
arsh277 <arshadashraf908@gmail.com>
2025-01-18 17:05:06 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const skills = document.querySelectorAll('.skill');
+skills.forEach(skill => {
+    skill.addEventListener('click', () => {
+        alert(`You clicked on ${skill.textContent}`);
+    });
+});
